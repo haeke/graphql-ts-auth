454ed7194da746b23ebf56fb3a95e43b6c2aee04
haeke <riverae2@student.wpunj.edu>
2019-05-01 14:06:24 -0400

    changed the User model entity so that it only contains an email and password, created a mutation responsible for creating a user with an email and hashed password.

--- src/resolvers.ts
+++ src/resolvers.ts
@@ -1,5 +1,22 @@
-export const resolvers = {
+import { IResolvers } from "graphql-tools";
+import * as bcrypt from "bcrypt";
+import { User } from "./entity/User";
+
+export const resolvers: IResolvers = {
   Query: {
     hello: () => "hello"
+  },
+  Mutation: {
+    register: async (_, { email, password }) => {
+      try {
+        // This function will create a user with an email and hashed password.
+        const hashedPassword = await bcrypt.hash(password, 10);
+        await User.create({ email, password: hashedPassword }).save();
+        return true;
+      } catch (error) {
+        console.error(error);
+        return false;
+      }
+    }
   }
 };
